a7f179caebb58888e8c723f442abae1d2dcd5ac9
shamoda <it19161648@my.sliit.lk>
2021-05-13 22:52:22 +0530

    call for papers and keynotes pages added

---
+++ src/component/KeyNotes/KeyNotes.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './KeyNotes.css'
+
+class KeyNotes extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+
+            </div>
+         );
+    }
+}
+ 
+export default KeyNotes;
